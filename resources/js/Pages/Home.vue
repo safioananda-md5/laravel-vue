4b26dbecdcabcc5133aeee86709054bb8aeeb09b
KHARISMA SAFIO ANANDA <safiopertama@gmail.com>
2026-01-05 13:17:12 +0700

    🛠️🪰Fix Bug: Cleaning unused file

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="p-10">
+        <h1 class="text-3xl font-bold text-blue-600">Project Baru Saya</h1>
+        <p>Ini adalah halaman kosong yang saya buat dari awal.</p>
+    </div>
+</template>
